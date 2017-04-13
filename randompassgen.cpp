650d5945c0dd71709dc42642bb2f325bb3f097e6
Nefos <nef0s@users.noreply.github.com>
2017-04-13 15:50:22 +0200

    Update randompassgen.cpp

--- randompassgen.cpp
+++ randompassgen.cpp
@@ -12,7 +12,6 @@
 #include <ctime>   //
 /////////////////////
 using namespace std;
-// Now I'm a ghost, I call your name, you look right through me.
 static const char alphanum[] =
 //////////////////////////////////
 "0123456789"                   //
@@ -24,10 +23,6 @@ int stringLength = sizeof(alphanum) - 1;
 
 char genRandom()
 {
-// I, yeah, I've been trying to fix my pride
-// But that shit's broken, that shit's broken
-// Lie , lie, l-lie, I try to hide
-// But now you know it
     return alphanum[rand() % stringLength];
 }
 
@@ -35,9 +30,6 @@ int main()
 {
     srand(time(0));
     for(int z=0; z < 21; z++)
-//That I'm at an all time
-//Low, low, low, low, low, low, low, low, low
-//Low, low, low, low, low, low, low, low
 
     {
         cout << genRandom();
